44073099a42c56aae46ca487cd85d920829aee5e
AndreLuis117 <andreluis2006@outlook.com>
2019-05-16 22:27:31 -0300

    first commit

---
+++ src/main/java/br/com/cybershop/CyberShopApplication.java
@@ -0,0 +1,13 @@
+package br.com.cybershop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CyberShopApplication.class, args);
+	}
+
+}

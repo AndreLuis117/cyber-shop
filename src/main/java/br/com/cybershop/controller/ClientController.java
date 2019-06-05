8ca87fc079f1c5a042c95396c50d4b26a1182178
AndreLuis117 <andreluis2006@outlook.com>
2019-06-04 21:39:15 -0300

    Construção de tela editar

--- src/main/java/br/com/cybershop/controller/ClientController.java
+++ src/main/java/br/com/cybershop/controller/ClientController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -37,6 +38,11 @@ public class ClientController {
 	{
 		clientService.save(client);
 		return new ModelAndView("redirect:/client");
+	}
+	
+	@GetMapping(value="/edit/{id}")
+	public ModelAndView edit(@PathVariable("id") Client client) {
+		return new ModelAndView("client/form","client",client);
 	}
 
 }

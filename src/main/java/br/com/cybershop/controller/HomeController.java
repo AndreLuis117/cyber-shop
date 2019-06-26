cebf45734bac8c4246df48c121cbbc031432e9b8
AndreLuis117 <andreluis2006@outlook.com>
2019-06-26 18:59:40 -0300

    Ajustes

--- src/main/java/br/com/cybershop/controller/HomeController.java
+++ src/main/java/br/com/cybershop/controller/HomeController.java
@@ -1,5 +1,20 @@
 package br.com.cybershop.controller;
 
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+
+@Controller
+@RequestMapping("/home")
 public class HomeController {
 
+	@GetMapping()
+	public ModelAndView index() {
+		 return new ModelAndView("home/index");
+	}
 }
